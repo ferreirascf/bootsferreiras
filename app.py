a38d3b3ad34bcdc18117aaf6cb5483e9888ee4a4
ferreirascf <matheusmartins09138@gmail.com>
2025-07-20 12:02:02 -0300

    Mensagem do que mudou

--- app.py
+++ app.py
@@ -416,6 +416,5 @@ def criar_banco():
     db.create_all()
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
